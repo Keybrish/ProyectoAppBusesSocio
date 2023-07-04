9bf178d6b286c02a7a1d611b793768b56534cbf4
Keybrish <keychanpa@gmail.com>
2023-07-04 11:54:00 -0500

    Funcionalidad pantalla login completada

--- appBusesDriver/app/src/main/java/dev/android/appbusesdriver/LoginActivity.kt
+++ appBusesDriver/app/src/main/java/dev/android/appbusesdriver/LoginActivity.kt
@@ -4,21 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
-import dev.android.appbusesdriver.databinding.ActivityWelcomeBinding
+import dev.android.appbusesdriver.databinding.ActivityLoginBinding
 
-class WelcomeActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityWelcomeBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java).apply {
-            }
-            startActivity(intent)
+        binding.btnBack.setOnClickListener {
+            finish()
         }
+
     }
 }
